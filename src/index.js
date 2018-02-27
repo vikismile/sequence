e3a1481257d8ca3dc7feca7f164d676dcd7b39e7
viktoria <vikaorabey@mail.ru>
2018-02-27 17:53:41 +0300

    upd complexity

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function longestConsecutiveLength(array) {
-    massiv = []
-    let f = 0
-    let prev = 0
     if(array.length==0) return 0
     if(array.length==1) return 1
+    /*massiv = []
+    let f = 0
+    let prev = 0
     else{
     for(let i = 0; i< array.length; i++){
       if(array.indexOf(array[i]+1) == -1){ continue}
@@ -19,8 +19,6 @@ module.exports = function longestConsecutiveLength(array) {
     }
     return f 
   }
-
-
   function pushmas (num) {
       massiv.push(num)
     if(array.indexOf(num+1) == -1){
@@ -28,5 +26,18 @@ module.exports = function longestConsecutiveLength(array) {
     }
    else pushmas(array[array.indexOf(num+1)])
 
-  }
+  }*/
+  a=0
+  b=1
+  array.sort(function(x,y){ return x-y;});
+  for(let i = 1; i< array.length; i++){
+    if(array[i]-array[i-1]==1){ 
+      b++
+    }
+    if(array[i]-array[i-1] > 1){
+      if(b>a) a=b
+      b=1
+    }
   } 
+  return a
+}
