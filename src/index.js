04d1b88e08705926062d1b65a7b2ef8f57dce0b8
viktoria <vikaorabey@mail.ru>
2018-02-27 17:20:10 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    massiv = []
+    let f = 0
+    let prev = 0
+    if(array.length==0) return 0
+    if(array.length==1) return 1
+    else{
+    for(let i = 0; i< array.length; i++){
+      if(array.indexOf(array[i]+1) == -1){ continue}
+        else{
+          prev=f
+        massiv=[]
+        pushmas(array[i])
+        f= massiv.length
+        if(f<prev){
+          f=prev
+        }
+      }
+    }
+    return f 
+  }
+
+
+  function pushmas (num) {
+   /* if(massiv.length>0){
+      if(array.indexOf(num+1))
+      massiv2.push(num)
+      pushmas(array[array.indexOf(num)])
+    }
+    else{*/
+      massiv.push(num)
+    if(array.indexOf(num+1) == -1){
+      return massiv
+    }
+   else pushmas(array[array.indexOf(num+1)])
+   // }
+   // if (massiv2.length>massiv.length) massiv=massiv2
+    //return massiv
+  }
+  } 
